d33de75f09004a44a46ca0c306ad930b0c03b126
Solinlee <solinlee@outlook.com>
2019-10-29 17:16:26 +0800

    springboot 注解不使用数据库

--- src/main/java/com/solin/wxsell/WxsellApplication.java
+++ src/main/java/com/solin/wxsell/WxsellApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //设置不使用数据库
 public class WxsellApplication {
 
     public static void main(String[] args) {
